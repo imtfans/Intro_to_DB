5fbc3e5c8b25f696e96170efef1235113593fcbe
godsent <godsentojugo@gmail.com>
2025-10-19 19:29:24 +0100

    add task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,14 @@
--- Task 4: Print full description of the Books table
+-- Task 4: Print full description of the Books table using INFORMATION_SCHEMA
 
-USE alx_book_store;
-
-SHOW COLUMNS FROM Books;
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_KEY, 
+    COLUMN_DEFAULT, 
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books';
