f21314e3358fe1b21044fa2cfbf6d136de66f187
godsent <godsentojugo@gmail.com>
2025-10-19 19:49:43 +0100

    add task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,8 +1,6 @@
--- Task 5: Insert a single customer into Customers table
+-- Task 5: Insert a single customer into the customer table
 
 USE alx_book_store;
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
-VALUES(2, 'Jane Doe', 'jdoe@example.com', '456 Joy St.');
-
